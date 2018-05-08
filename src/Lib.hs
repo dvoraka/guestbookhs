21197dfdb61c55caa4ce1a79535e8b21d90a0ba0
dvoraka <alen.dvorak@gmail.com>
2018-05-08 22:50:14 +0200

    Add JSON example

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Lib
     ( testFunc
@@ -6,9 +7,19 @@ module Lib
     ) where
 
 import Data.Monoid ((<>))
+import GHC.Generics
 
 import Web.Scotty
+import Data.Aeson (FromJSON, ToJSON)
 
+
+data Greeting = Greeting { message :: String } deriving (Show, Generic)
+
+instance ToJSON Greeting
+instance FromJSON Greeting
+
+defaultGreeting :: Greeting
+defaultGreeting = Greeting { message = "Hello!" }
 
 testFunc :: IO ()
 testFunc = putStrLn "Test function"
@@ -18,6 +29,7 @@ routes = do
   get "/hello" hello
   get "/info" info
   get "/greeting/:name" greeting
+  get "/test" jsonTest
 
 hello :: ActionM ()
 hello = do
@@ -32,3 +44,6 @@ greeting = do
   nameParam <- param "name"
   let name = if nameParam == "" then "buddy" else nameParam
   text ("hello " <> name <> "!")
+
+jsonTest :: ActionM ()
+jsonTest = json defaultGreeting
